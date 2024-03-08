cf4cf2cea4ff8a5287e20bf4d037e0da7ab26b92
J SHYAM NARAYANAN <145639384+Shyamnarayananj@users.noreply.github.com>
2024-03-08 12:46:33 +0530

    Add files via upload

---
+++ testvoice.py
@@ -0,0 +1,56 @@
+
+import pyttsx3
+
+'''import subprocess
+import wolframalpha
+import pyttsx3
+import tkinter
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import pyjokes
+import feedparser
+import smtplib
+import ctypes
+import time
+import requests
+import shutil
+from twilio.rest import Client
+from clint.textui import progress
+from ecapture import ecapture as ec
+from bs4 import BeautifulSoup
+import win32com.client as wincl
+from urllib.request import urlopen'''
+
+engine = pyttsx3.init()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+ 
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>= 0 and hour<12:
+        speak("Good Morning Sir !")
+  
+    elif hour>= 12 and hour<18:
+        speak("Good Afternoon Sir !")  
+  
+    else:
+        speak("Good Evening Sir !") 
+  
+    assname =("Jarvis 1 point o")
+    speak("I am your Assistant")
+    speak(assname)
+
+speak("How did you handle disagreements with your manager?")
+
+
+
+    
